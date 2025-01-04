6b71792e947367b0b90e0f0c549cb557cb9a9323
matle-br <mateo.lebrassancho@gmail.com>
2025-01-04 17:08:44 +0100

    I am working on mariadb

--- srcs/requirements/mariadb/conf/start.sh
+++ srcs/requirements/mariadb/conf/start.sh
@@ -22,24 +22,22 @@
 
 # -------------------------------------------------------------
 
-echo "Démarrage du service MariaDB..."
-service mariadb start;
+echo "Starting MariaDB..."
+# service mariadb start
+sleep 5
 
 echo "Initialisation de la base de données MariaDB..."
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${MARIADB_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${MARIADB_USER}\`@'localhost' IDENTIFIED BY '${MARIADB_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${MARIADB_DATABASE}\`.* TO \`${MARIADB_USER}\`@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';"
-mysql -e "FLUSH PRIVILEGES;"
 
-mysqladmin -u root -p$MARIADB_ROOT_PASSWORD shutdown
-exec mysqld_safe
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MARIADB_DATABASE}\`;"
+mariadb -e "CREATE USER IF NOT EXISTS \`${MARIADB_USER}\`@'localhost' IDENTIFIED BY '${MARIADB_PASSWORD}';"
+mariadb -e "GRANT ALL PRIVILEGES ON \`${MARIADB_DATABASE}\`.* TO \`${MARIADB_USER}\`@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';"
+mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';"
+mariadb -e "FLUSH PRIVILEGES;"
 
+mysqladmin -u root -p shutdown
+
+# service mariadb stop;
 echo "La base de données MariaDB est en place !"
+# exec mysqld_safe
 
 # ------------------------------------------------------------
-
-
-# mariadb -e "CREATE USER 'admin'@'%' IDENTIFIED BY 'mot_de_passe';"
-# mariadb -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION;"
-# mariadb -e "FLUSH PRIVILEGES;"

1ce962a76988c87fd4331105898e983e0b11012a
matle-br <mateo.lebrassancho@gmail.com>
2025-01-06 17:32:51 +0100

    I improved MariaDB but it does not work

--- srcs/requirements/mariadb/conf/start.sh
+++ srcs/requirements/mariadb/conf/start.sh
@@ -1,43 +1,35 @@
 #!/bin/bash
 
-# echo "Démarrage du script pour initialiser la base de données MariaDB..."
-
-# service mariadb start;
-
-# mariadb -u root
-
-# mariadb -e "CREATE DATABASE IF NOT EXISTS ${MARIADB_DATABASE};"
-
-# mariadb -e "USE ${MARIADB_DATABASE}";
-
-# mariadb -e "CREATE OR REPLACE USER '${MARIADB_USER}'@'localhost' IDENTIFIED BY '${MARIADB_PASSWORD}';"
-# mariadb -e "CREATE OR REPLACE USER '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';"
-
-# mariadb -e "GRANT ALL PRIVILEGES ON *'${MARIADB_DATABASE}'.* TO '${MARIADB_USER}'@'localhost' IDENTIFIED BY '${MARIADB_PASSWORD}';"
-# mariadb -e "GRANT ALL PRIVILEGES ON *'${MARIADB_DATABASE}'.* TO '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';"
-
-# mariadb -e "FLUSH ${MARIADB_DATABASE};"
-
-# mariadb -e "EXIT;"
-
-# -------------------------------------------------------------
-
 echo "Starting MariaDB..."
-# service mariadb start
+mariadbd-safe --datadir=/var/lib/mysql &
+# service mysqld start
 sleep 5
 
 echo "Initialisation de la base de données MariaDB..."
 
-mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MARIADB_DATABASE}\`;"
-mariadb -e "CREATE USER IF NOT EXISTS \`${MARIADB_USER}\`@'localhost' IDENTIFIED BY '${MARIADB_PASSWORD}';"
-mariadb -e "GRANT ALL PRIVILEGES ON \`${MARIADB_DATABASE}\`.* TO \`${MARIADB_USER}\`@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';"
-mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';"
-mariadb -e "FLUSH PRIVILEGES;"
+mysqld "CREATE DATABASE IF NOT EXISTS \`${MARIADB_DATABASE}\`;"
+mysqld "CREATE USER IF NOT EXISTS \`${MARIADB_USER}\`@'localhost' IDENTIFIED BY '${MARIADB_PASSWORD}';"
+mysqld "GRANT ALL PRIVILEGES ON \`${MARIADB_DATABASE}\`.* TO \`${MARIADB_USER}\`@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';"
+mysqld "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';"
+mysqld "FLUSH PRIVILEGES;"
 
-mysqladmin -u root -p shutdown
+mysqladmin -u root -p"${MARIADB_ROOT_PASSWORD}" shutdown
+# service mysqld stop
 
-# service mariadb stop;
 echo "La base de données MariaDB est en place !"
-# exec mysqld_safe
+exec mariadbd-safe --datadir=/var/lib/mysql
 
-# ------------------------------------------------------------
+# ----------------------------------------------------
+
+# echo "Initialisation de la base de données MariaDB..."
+
+# mysql -e "CREATE DATABASE IF NOT EXISTS mydb;"
+# mysql -e "CREATE USER IF NOT EXISTS 'myuser'@'localhost' IDENTIFIED BY 'password';"
+# mysql -e "GRANT ALL PRIVILEGES ON 'mydb' TO 'myuser'@'%' IDENTIFIED BY 'password';"
+# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'rootpassword';"
+# mysql -e "FLUSH PRIVILEGES;"
+
+# mysqladmin -u root -p shutdown
+
+# echo "La base de données MariaDB est en place !"
+# exec mysqld_safe
